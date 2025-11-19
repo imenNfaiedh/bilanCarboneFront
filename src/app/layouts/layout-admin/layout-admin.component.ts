685d3322045ffc4e85815607c03da46f799072c2
imennfaidh8@gmail.com <imennfaidh8@gmail.com>
2025-11-19 15:47:44 +0100

    scroll

--- src/app/layouts/layout-admin/layout-admin.component.ts
+++ src/app/layouts/layout-admin/layout-admin.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
+import {PerimetreComponent} from "../../modules/admin/perimetre/perimetre/perimetre.component";
+import {ObjectifsComponent} from "../../modules/admin/objectifs/objectifs.component";
 
 @Component({
   selector: 'app-layout-admin',
@@ -7,11 +9,24 @@ import {RouterLink, RouterLinkActive, RouterOutlet} from "@angular/router";
   imports: [
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    PerimetreComponent,
+    ObjectifsComponent
   ],
   templateUrl: './layout-admin.component.html',
   styleUrl: './layout-admin.component.scss'
 })
 export class LayoutAdminComponent {
+  @ViewChild('objectifsSection') objectifsSection!: ElementRef;
+  @ViewChild('perimetreSection') perimetreSection!: ElementRef;
 
+  scrollTo(section: 'objectifs' | 'perimetre') {
+    if (section === 'objectifs') {
+      this.objectifsSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    } else {
+      this.perimetreSection.nativeElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
+
+
